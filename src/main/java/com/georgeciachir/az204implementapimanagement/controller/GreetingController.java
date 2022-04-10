db0b8cc190377e440a8161e031c75c915e6bdf45
GeorgeCiachir <george.ciachir@yahoo.com>
2022-04-10 11:52:16 +0300

    change controller

--- src/main/java/com/georgeciachir/az204implementapimanagement/controller/GreetingController.java
+++ src/main/java/com/georgeciachir/az204implementapimanagement/controller/GreetingController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -19,8 +20,8 @@ public class GreetingController {
      * This endpoint produces JSON, but for this Azure module I am using a <a href="https://docs.microsoft.com/en-us/azure/api-management/api-management-transformation-policies#ConvertJSONtoXML">json-to-xml transforming policy</a>,
      * so the response will be transformed by the Azure policy
      */
-    @GetMapping(value = "/hello/{name}")
-    public GreetingDto sayHello(@PathVariable String name) {
+    @GetMapping(value = "/hello")
+    public GreetingDto sayHello(@RequestParam String name) {
         LOG.info("Greeting [{}]", name);
 
         return new GreetingDto(name, String.format("Hello, %s!", name), LocalDateTime.now());
